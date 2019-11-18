2fa10cc93ed58a7303a405cdcdfb5004a7cbb2be
Anthony <GerudoValley@Tal-Tal-Heights.local>
2019-11-18 15:15:42 -0500

    spellschecker library
    Better library for our purposes. Implemented a way to add and remove words for customization.

--- Backend/Testing.py
+++ Backend/Testing.py
@@ -1,4 +1,35 @@
-from textblob import TextBlob
+from spellchecker import *
 
-text = TextBlob("I thinke I jus found a way to ruu this wihhout TensorFlow")
-print(text.correct())
+spell = SpellChecker('en')
+
+
+def add_word(new):
+    # word is a list of words we wanna add
+    spell.word_frequency.load_words(new)
+
+
+def remove_word(old):
+    # word is a list of words we wanna add
+    spell.word_frequency.remove_words(old)
+
+
+add_word(["TensorFlow", "doesn't"])
+
+remove_word(["wee"])
+
+string = "I thinnk thst wee foud a way that desn't require TensorFlow"
+string = string.split()
+print(string)
+
+# find those words that may be misspelled
+misspelled = spell.unknown(string)
+
+for word in misspelled:
+    # Get the one `most likely` answer
+    print(spell.correction(word))
+
+    # Get a list of `likely` options
+    print(spell.candidates(word))
+
+# This model only touches words that actually have typos, context isn't really factored in.
+# Also the output is just the corrections, not the whole sentence.
