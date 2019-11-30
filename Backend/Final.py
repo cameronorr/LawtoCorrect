08bebc01646dc1e8356247448b98ffa3180cf938
Anthony <GerudoValley@Tal-Tal-Heights.local>
2019-11-30 13:46:17 -0500

    Moved Testing to Final

---
+++ Backend/Final.py
@@ -0,0 +1,24 @@
+from spellchecker import *
+
+spell = SpellChecker('en')
+Elissa_dict = './dictionary.txt'
+spell.word_frequency.load_text_file(Elissa_dict, 'utf-8')
+
+dirty = open('./bad_words.txt', 'r', encoding='utf-8-sig')
+junk = dirty.read().splitlines()
+dirty.close()
+spell.word_frequency.remove_words(junk)
+
+
+def new():
+    current_string = open('./keyLog.txt', 'r+', encoding='utf-8-sig')
+    string = current_string.read()
+    current_string.truncate(0)
+    correct = spell.correction(string)
+    if len(string) != 0 and string != correct:
+        print(correct)
+        return correct
+    print("** Word Is Already Correct**")
+
+
+new()
--- Backend/Testing.py
+++ Backend/Testing.py
@@ -1,45 +1,63 @@
-from spellchecker import *
-
-spell = SpellChecker('en')
-# English language spellchecker
-
-Elissa_dict = './dictionary.txt'
-spell.word_frequency.load_text_file(Elissa_dict, 'utf-8')
-# txt file containing "new" corrections
-
-
-def cleanse():
-    dirty = open('./bad_words.txt', 'r', encoding='utf-8-sig')
-    junk = dirty.read().splitlines()
-    dirty.close()
-    spell.word_frequency.remove_words(junk)
-# removes all words we don't want as corrections
-
-
-cleanse()
-
-current_string = open('./input.txt', 'r', encoding='utf-8-sig')
-string = current_string.read()
-print("original text: ", string)
-string = string.split()
-current_string.close()
-
-correct = ""
-for word in string:
-    correct += str(spell.correction(word)) + " "
-    
-
-print("corrected text: ", correct.strip())
-# find those words that may be misspelled
-# misspelled = spell.unknown(string)
-
-# for word in misspelled:
-#     # Get the one "most likely" answer
-#     print(spell.correction(word))
+# from spellchecker import *
 #
-#     # Get a list of "likely" options
-#     print(spell.candidates(word))
+# spell = SpellChecker('en')
+# Elissa_dict = './dictionary.txt'
+# spell.word_frequency.load_text_file(Elissa_dict, 'utf-8')
+#
+# dirty = open('./bad_words.txt', 'r', encoding='utf-8-sig')
+# junk = dirty.read().splitlines()
+# dirty.close()
+# spell.word_frequency.remove_words(junk)
 
-# This model only touches words that actually have typos, context isn't really factored in.
-# Also the output is just the corrections, not the whole sentence.
-# There are other methods I gotta play with though.
+
+# def cleanse():
+#     dirty = open('./bad_words.txt', 'r', encoding='utf-8-sig')
+#     junk = dirty.read().splitlines()
+#     dirty.close()
+#     spell.word_frequency.remove_words(junk)
+#
+#
+# # removes all words we don't want as corrections
+#
+# cleanse()
+
+
+def old():
+    current_string = open('./input.txt', 'r', encoding='utf-8-sig')
+    string = current_string.read()
+    print("original text: ", string)
+    string = string.split()
+    current_string.close()
+
+    correct = ""
+    for word in string:
+        correct += str(spell.correction(word)) + " "
+
+    print("corrected text: ", correct.strip())
+    # find those words that may be misspelled
+    misspelled = spell.unknown(string)
+
+    for word in misspelled:
+        # Get the one "most likely" answer
+        print("misspelled: ", spell.correction(word))
+
+        # Get a list of "likely" options
+        # print(spell.candidates(word))
+
+    # This model only touches words that actually have typos, context isn't really factored in.
+    # Also the output is just the corrections, not the whole sentence.
+    # There are other methods I gotta play with though.
+
+
+# def new():
+#     current_string = open('./input.txt', 'r+', encoding='utf-8-sig')
+#     string = current_string.read()
+#     current_string.truncate(0)
+#     correct = spell.correction(string)
+#     if len(string) != 0 and string != correct:
+#         print(correct)
+#         return correct
+#     print("** Word Is Already Correct**")
+#
+#
+# new()
