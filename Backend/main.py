c6495f192bd5af831650ed0b9f1105e036909c3b
Anthony <GerudoValley@Tal-Tal-Heights.local>
2019-12-02 15:53:02 -0500

    main has contents of Atom

--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,15 @@
-from spellchecker import *
+from spellchecker import SpellChecker
+from pynput.keyboard import Key, Listener
+import logging
+
+from pyautogui import typewrite, hotkey
 
 spell = SpellChecker('en')
+
+
 Elissa_dict = './dictionary.txt'
 spell.word_frequency.load_text_file(Elissa_dict, 'utf-8')
+
 
 dirty = open('./bad_words.txt', 'r', encoding='utf-8-sig')
 junk = dirty.read().splitlines()
@@ -10,12 +17,68 @@ dirty.close()
 spell.word_frequency.remove_words(junk)
 
 
-def new():
-    current_string = open('./input.txt', 'r+', encoding='utf-8-sig')
+def autocorrect():
+    current_string = open('./keyLog.txt', 'r+', encoding='utf-8-sig')
     string = current_string.read()
     current_string.truncate(0)
     correct = spell.correction(string)
     if len(string) != 0 and string != correct:
-        print(correct)
+        # print(correct)
         return correct
-    print("** Word Is Already Correct**")
+    # else:
+    #     print("** Word Is Already Correct**")
+    #     return
+
+
+log_dir = "/Users/GerudoValley/Desktop/Ratz4Lyfe/"
+logging.basicConfig(filename=(log_dir + "keyLog.txt"), level=logging.DEBUG, format='%(message)s')
+
+
+keys = []
+shifted = False
+
+
+def in_alphabet(key):
+    return 32 < ord(key) < 127
+
+
+def on_release(key):
+    global keys, shifted, corrected
+    try:
+        if key == Key.space:
+            write_file(keys)
+            corrected = autocorrect()
+            if not corrected:
+                corrected = keys
+            hotkey('del','ctrl', 'del')
+            typewrite(corrected)
+            keys = []
+            return
+        elif key == Key.backspace:
+            keys.pop()
+            return
+        elif key == Key.shift:
+            shifted = True
+            return
+
+        # print(str(key).replace("'", ""))
+        if shifted and in_alphabet(str(key).replace("'", "")):
+            keys.append(str(key).upper())
+            shifted = False
+        elif shifted and not in_alphabet(str(key).replace("'", "")):
+            shifted = False
+        elif in_alphabet(str(key).replace("'", "")):
+            keys.append(str(key))
+    except:
+        pass
+
+def write_file(keys):
+    with open("keyLog.txt", "a") as f:
+        f.write(" ")
+        for key in keys:
+            k = str(key).replace("'", "")
+            f.write(k)
+
+
+with Listener(on_release=on_release) as listener:
+    listener.join()
